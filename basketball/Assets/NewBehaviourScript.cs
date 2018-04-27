5cfdb9b062173972b64f8371fa3b26b2ab3449fa
YURU <rubystage@gmail.com>
2018-04-27 12:27:09 +0800

    no message

---
+++ basketball/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    GameObject arrow;
+    public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+        arrow = GameObject.Find("arrow");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("left"))
+            arrow.transform.Rotate(0, 2, 0);
+        if (Input.GetKey("right"))
+            arrow.transform.Rotate(0, -2, 0);
+
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject copyball = Instantiate(ball);
+            copyball.GetComponent<Rigidbody>().AddForce(arrow.transform.forward*12, ForceMode.Impulse);
+        }
+    }
+}
